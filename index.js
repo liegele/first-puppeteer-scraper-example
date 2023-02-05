85de18728ef9b14205ffcdb8f6182f54b49fda34
liegele <liegele@gmail.com>
2023-02-04 23:10:05 -0300

    Busca quotes na primeira página e clica no botão next.

--- index.js
+++ index.js
@@ -24,22 +24,46 @@ const getQuotes = async () => {
 
   //Get page data
   const quotes = await page.evaluate(() => {
+    //Fetch all elements from page with class "quote"
+    const quoteList = document.querySelectorAll('.quote');
     //Fetch the first element with class "quote"
-    const quote = document.querySelector('.quote');
+    // const quote = document.querySelector('.quote');
 
-    //Fetch the sub-elements from the previously fetched quote element
-    //Get the displayed text and returned it ('innerText')
-    const text = quote.querySelector('.text').innerText;
-    const author = quote.querySelector('.author').innerText;
+    //Convert quoteList to an iterable array - Array.from()
+    //For each quote fetch the text and author
+    return Array.from(quoteList).map((quote) => {
+      //Fetch the sub-elements from the previously fetched quote element
+      //Get the displayed text and returned it ('innerText')
+      const text = quote.querySelector('.text').innerText;
+      const author = quote.querySelector('.author').innerText;
 
-    return { text, author };
+      return { text, author };
+    });
   });
 
   //Display quotes
   console.log(quotes);
 
+  // Wait and click on first result of '.pager > .next > a'
+  // const nextButton = '.pager > .next > a';
+  // await page.waitForSelector(nextButton);
+  // await page.click(nextButton);
+
+  // Wait and click on first result of '.pager > .next > a'
+  await page.evaluate(() => {
+    document.querySelector('.pager > .next > a').click();
+  });
+
+  //Click on the NEXT button
+  // await page.click('.pager > .next > a');
+
+  // const [response] = await Promise.all([
+  //   page.waitForNavigation(),
+  //   page.click('.pager > .next > a'),
+  // ]);
+
   /* //Close tab
-  await page.close();
+  await page.close();ext
   //Close the browser
   await browser.close(); */
 };
